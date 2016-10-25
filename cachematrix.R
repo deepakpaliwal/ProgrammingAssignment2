6c087218870496e3d7711ab5d29124ed87f8c343
Deepak Paliwal <deepak.paliwal@gmail.com>
2016-10-25 15:03:37 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This function create an matrix and 
+## cash its inverse into global env for future fetching
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+makeCacheMatrix <-function(x = matrix()) {
+  m <- NULL 
   set <- function(y) {
     x <<- y
     m <<- NULL
@@ -19,6 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## This function calculate inverse if it's not already
+## there in the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
